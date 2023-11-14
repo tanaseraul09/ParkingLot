d83222c0e0b620dcd75b30b9d2a7153b03dc3188
tanaseraul09 <127606564+tanaseraul09@users.noreply.github.com>
2023-11-14 10:43:36 +0200

    Created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/car.java
@@ -0,0 +1,51 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class car {
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    private String licensePlate;
+
+    @Basic
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    private String parkingSpot;
+
+    @Basic
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    private User owner;
+
+    @ManyToOne
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
